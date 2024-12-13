2b9d835470c511e54f6ecc478fa3967e98a7f2d4
Kashif Qamar <145191836+Kashif-Qamar1120@users.noreply.github.com>
2024-12-13 11:29:34 +0500

    gov-car database

--- gov-car.sql
+++ gov-car.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2024 at 07:05 AM
+-- Generation Time: Dec 13, 2024 at 07:28 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
